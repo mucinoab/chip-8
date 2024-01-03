b0e5d9004b1e8584a7a76928de0fe727a54778f4
Bruno A. Muciño <mucinoab@gmail.com>
2024-01-02 21:00:31 -0600

    Turn project into a library

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+mod opcode;
+
+use opcode::OpCode;
+
 use rand::Rng;
 
-const TICKS_PER_FRAME: usize = 10;
-const SCREEN_WIDTH: usize = 64;
-const SCREEN_HEIGHT: usize = 32;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+pub const TICKS_PER_FRAME: usize = 10;
+
 const FONTSET: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -22,7 +27,7 @@ const FONTSET: [u8; 80] = [
     0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
-struct Chip8 {
+pub struct Chip8 {
     /// Index
     idx: usize,
 
@@ -36,31 +41,32 @@ struct Chip8 {
     /// Registers 0x0 - 0xF
     v: [u8; 16],
 
-    /// 0x200-0xFFF - Program ROM and work RAM
+    /// Program ROM and work RAM
+    /// 0x200 - 0xFFF
     mem: [u8; 4096],
 
     /// Monochromatic screen
-    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    pub screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
 
     delay_timer: u8,
     sound_timer: u8,
     rng: rand::rngs::ThreadRng,
 
-    /// Keypad             Keyboard
-    /// +-+-+-+-+          +-+-+-+-+
-    /// |1|2|3|C|          |1|2|3|4|
-    /// +-+-+-+-+          +-+-+-+-+
-    /// |4|5|6|D|          |Q|W|E|R|
-    /// +-+-+-+-+    =>    +-+-+-+-+
-    /// |7|8|9|E|          |A|S|D|F|
-    /// +-+-+-+-+          +-+-+-+-+
-    /// |A|0|B|F|          |Z|X|C|V|
-    /// +-+-+-+-+          +-+-+-+-+
+    /// Keypad           Keyboard
+    /// +-+-+-+-+        +-+-+-+-+
+    /// |1|2|3|C|        |1|2|3|4|
+    /// +-+-+-+-+        +-+-+-+-+
+    /// |4|5|6|D|        |Q|W|E|R|
+    /// +-+-+-+-+   =>   +-+-+-+-+
+    /// |7|8|9|E|        |A|S|D|F|
+    /// +-+-+-+-+        +-+-+-+-+
+    /// |A|0|B|F|        |Z|X|C|V|
+    /// +-+-+-+-+        +-+-+-+-+
     keypad: [bool; 16],
 }
 
 impl Chip8 {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let mut c8 = Self {
             idx: 0,
             pc: 0x200,
@@ -81,15 +87,18 @@ impl Chip8 {
         c8
     }
 
-    fn load(&mut self, game_path: &str) {
-        let s = std::fs::read(game_path).expect("Unable to find given game");
+    pub fn load(&mut self, game_rom: &[u8]) {
+        let start = 0x200;
+        let end = start + game_rom.len();
 
-        for (idx, &op) in s.iter().enumerate() {
-            self.mem[idx + 0x200] = op;
-        }
+        self.mem[start..end].copy_from_slice(game_rom);
     }
 
-    fn cycle(&mut self) {
+    pub fn reset(&mut self) {
+        *self = Chip8::new();
+    }
+
+    pub fn cycle(&mut self) {
         // Fetch Opcode
         let op_a = self.mem[self.pc as usize];
         let op_b = self.mem[self.pc as usize + 1];
@@ -112,31 +121,44 @@ impl Chip8 {
         self.sound_timer = self.sound_timer.saturating_sub(1);
     }
 
-    fn draw_graphics(&self) {
-        let mut screen = vec![vec![' '; SCREEN_WIDTH]; SCREEN_HEIGHT];
+    pub fn key2btn(key: &str) -> Option<usize> {
+        match key {
+            "1" => Some(0x1),
+            "2" => Some(0x2),
+            "3" => Some(0x3),
+            "4" => Some(0xC),
+            "q" => Some(0x4),
+            "w" => Some(0x5),
+            "e" => Some(0x6),
+            "r" => Some(0xD),
+            "a" => Some(0x7),
+            "s" => Some(0x8),
+            "d" => Some(0x9),
+            "f" => Some(0xE),
+            "z" => Some(0xA),
+            "x" => Some(0x0),
+            "c" => Some(0xB),
+            "v" => Some(0xF),
+            _ => None,
+        }
+    }
 
+    pub fn keypress(&mut self, key: usize, pressed: bool) {
+        self.keypad[key] = pressed;
+    }
+
+    pub fn pixel_cooridinates(&self) -> Vec<(usize, usize)> {
         self.screen
             .iter()
             .enumerate()
             .filter(|(_, p)| **p)
-            .map(|(i, _)| i)
-            .for_each(|i| {
+            .map(|(i, _)| {
                 let x = i / SCREEN_WIDTH;
                 let y = i % SCREEN_WIDTH;
 
-                screen[x][y] = '0';
-            });
-
-        let mut s = String::with_capacity((SCREEN_HEIGHT * SCREEN_WIDTH) + SCREEN_HEIGHT);
-
-        for row in screen {
-            s.extend(row.iter());
-            s.push('\n');
-        }
-
-        print!("{esc}[2J{esc}[1;1H{s}", esc = 27 as char);
-
-        // std::io::stdin().read_line(&mut String::new());
+                (x, y)
+            })
+            .collect()
     }
 
     /// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
@@ -314,166 +336,16 @@ impl Chip8 {
             }
         }
 
-        self.v[0xF] = collision as _;
+        self.v[0xF] = collision as u8;
     }
 
-    fn run(&mut self) -> ! {
-        loop {
-            for _ in 0..TICKS_PER_FRAME {
-                self.cycle();
-            }
-
-            self.draw_graphics();
-            std::thread::sleep(std::time::Duration::from_millis(16))
-        }
-    }
-
-    fn clear_screen(&mut self) {
+    pub fn clear_screen(&mut self) {
         self.screen.fill(false);
     }
 }
 
-/// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.0
-#[derive(Debug, PartialEq)]
-enum OpCode {
-    Cls,              // 00E0 - CLS
-    Ret,              // 00EE - RET
-    Jp(u16),          // 1nnn - JP addr
-    Call(u16),        // 2nnn - CALL addr
-    SeVx(u8, u16),    // 3xkk - SE Vx, value
-    SNeVx(u8, u16),   // 4xkk - SNE Vx, value
-    SeVxVy(u8, u8),   // 5xy0 - SE Vx, Vy
-    LdVx(u8, u16),    // 6xkk - LD Vx, value
-    AddVx(u8, u16),   // 7xkk - ADD Vx, value
-    LdVxVy(u8, u8),   // 8xy0 - LD Vx, Vy
-    Or(u8, u8),       // 8xy1 - OR Vx, Vy
-    And(u8, u8),      // 8xy2 - AND Vx, Vy
-    Xor(u8, u8),      // 8xy3 - XOR Vx, Vy
-    Add(u8, u8),      // 8xy4 - ADD Vx, Vy
-    Sub(u8, u8),      // 8xy5 - SUB Vx, Vy
-    Shr(u8),          // 8xy6 - SHR Vx {, Vy}
-    SubN(u8, u8),     // 8xy7 - SUBN Vx, Vy
-    Shl(u8),          // 8xyE - SHL Vx {, Vy}
-    SeNeVxVy(u8, u8), // 9xy0 - SNE Vx, Vy
-    LdI(u16),         // Annn - LD I, addr
-    JpV0(u16),        // Bnnn - JP V0, addr
-    Rnd(u8, u8),      // Cxkk - RND Vx, value
-    Drw(u8, u8, u8),  // Dxyn - DRW Vx, Vy, nibble
-    Skp(u8),          // Ex9E - SKP Vx
-    SkNp(u8),         // ExA1 - SKNP Vx
-    LdVxDt(u8),       // Fx07 - LD Vx, DT
-    LdVxK(u8),        // Fx0A - LD Vx, K
-    LdDtVx(u8),       // Fx15 - LD DT, Vx
-    LdStVx(u8),       // Fx18 - LD ST, Vx
-    AddIVx(u8),       // Fx1E - ADD I, Vx
-    LdFVx(u8),        // Fx29 - LD F, Vx
-    LdBVx(u8),        // Fx33 - LD B, Vx
-    LdIVx(u8),        // Fx55 - LD [I], Vx
-    LdVxI(u8),        // Fx65 - LD Vx, [I]
-}
-
-impl OpCode {
-    fn decode(raw_a: u8, raw_b: u8) -> Self {
-        let opcode: u16 = (raw_a as u16) << 8 | (raw_b as u16);
-
-        match opcode & 0xF000 {
-            0x0000 => match opcode & 0x000F {
-                0x0000 => Self::Cls,
-                0x000E => Self::Ret,
-                _ => unreachable!("Invalid Op {:#x}", opcode),
-            },
-            0x1000 => Self::Jp(opcode & 0x0FFF),
-            0x2000 => Self::Call(opcode & 0x0FFF),
-            0x3000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let value = opcode & 0x00FF;
-                Self::SeVx(vx, value)
-            }
-            0x4000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let value = opcode & 0x00FF;
-                Self::SNeVx(vx, value)
-            }
-            0x5000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let vy = ((opcode & 0x00F0) >> 4) as u8;
-                Self::SeVxVy(vx, vy)
-            }
-            0x6000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let value = opcode & 0x00FF;
-                Self::LdVx(vx, value)
-            }
-            0x7000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let value = opcode & 0x00FF;
-                Self::AddVx(vx, value)
-            }
-            0x8000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let vy = ((opcode & 0x00F0) >> 4) as u8;
-
-                match opcode & 0x000F {
-                    0x0000 => Self::LdVxVy(vx, vy),
-                    0x0001 => Self::Or(vx, vy),
-                    0x0002 => Self::And(vx, vy),
-                    0x0003 => Self::Xor(vx, vy),
-                    0x0004 => Self::Add(vx, vy),
-                    0x0005 => Self::Sub(vx, vy),
-                    0x0006 => Self::Shr(vx),
-                    0x0007 => Self::SubN(vx, vy),
-                    0x000E => Self::Shl(vx),
-                    _ => unreachable!("Invalid Op {:#x}", opcode),
-                }
-            }
-            0x9000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let vy = ((opcode & 0x00F0) >> 4) as u8;
-                Self::SeNeVxVy(vx, vy)
-            }
-            0xA000 => Self::LdI(opcode & 0x0FFF),
-            0xB000 => Self::JpV0(opcode & 0x0FFF),
-            0xC000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let value = (opcode & 0x00FF) as u8;
-                Self::Rnd(vx, value)
-            }
-            0xD000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                let vy = ((opcode & 0x00F0) >> 4) as u8;
-                let n = (opcode & 0x000F) as u8;
-                Self::Drw(vx, vy, n)
-            }
-            0xE000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                match opcode & 0x00FF {
-                    0x009E => Self::Skp(vx),
-                    0x00A1 => Self::SkNp(vx),
-                    _ => unreachable!("Invalid Op {:#x} {:#x}", opcode, opcode & 0x00FF),
-                }
-            }
-            0xF000 => {
-                let vx = ((opcode & 0x0F00) >> 8) as u8;
-                match opcode & 0x00FF {
-                    0x0007 => Self::LdVxDt(vx),
-                    0x000A => Self::LdVxK(vx),
-                    0x0015 => Self::LdDtVx(vx),
-                    0x0018 => Self::LdStVx(vx),
-                    0x001E => Self::AddIVx(vx),
-                    0x0029 => Self::LdFVx(vx),
-                    0x0033 => Self::LdBVx(vx),
-                    0x0055 => Self::LdIVx(vx),
-                    0x0065 => Self::LdVxI(vx),
-                    _ => unreachable!("Invalid Op {:#x}", opcode),
-                }
-            }
-            _ => unreachable!(),
-        }
+impl Default for Chip8 {
+    fn default() -> Self {
+        Self::new()
     }
-}
-
-fn main() {
-    let mut c8 = Chip8::new();
-    c8.load("./br8kout.ch8");
-    c8.run();
 }
---
+++ src/opcode.rs
@@ -0,0 +1,138 @@
+/// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.0
+#[derive(Debug, PartialEq)]
+pub enum OpCode {
+    Cls,              // 00E0 - CLS
+    Ret,              // 00EE - RET
+    Jp(u16),          // 1nnn - JP addr
+    Call(u16),        // 2nnn - CALL addr
+    SeVx(u8, u16),    // 3xkk - SE Vx, value
+    SNeVx(u8, u16),   // 4xkk - SNE Vx, value
+    SeVxVy(u8, u8),   // 5xy0 - SE Vx, Vy
+    LdVx(u8, u16),    // 6xkk - LD Vx, value
+    AddVx(u8, u16),   // 7xkk - ADD Vx, value
+    LdVxVy(u8, u8),   // 8xy0 - LD Vx, Vy
+    Or(u8, u8),       // 8xy1 - OR Vx, Vy
+    And(u8, u8),      // 8xy2 - AND Vx, Vy
+    Xor(u8, u8),      // 8xy3 - XOR Vx, Vy
+    Add(u8, u8),      // 8xy4 - ADD Vx, Vy
+    Sub(u8, u8),      // 8xy5 - SUB Vx, Vy
+    Shr(u8),          // 8xy6 - SHR Vx {, Vy}
+    SubN(u8, u8),     // 8xy7 - SUBN Vx, Vy
+    Shl(u8),          // 8xyE - SHL Vx {, Vy}
+    SeNeVxVy(u8, u8), // 9xy0 - SNE Vx, Vy
+    LdI(u16),         // Annn - LD I, addr
+    JpV0(u16),        // Bnnn - JP V0, addr
+    Rnd(u8, u8),      // Cxkk - RND Vx, value
+    Drw(u8, u8, u8),  // Dxyn - DRW Vx, Vy, nibble
+    Skp(u8),          // Ex9E - SKP Vx
+    SkNp(u8),         // ExA1 - SKNP Vx
+    LdVxDt(u8),       // Fx07 - LD Vx, DT
+    LdVxK(u8),        // Fx0A - LD Vx, K
+    LdDtVx(u8),       // Fx15 - LD DT, Vx
+    LdStVx(u8),       // Fx18 - LD ST, Vx
+    AddIVx(u8),       // Fx1E - ADD I, Vx
+    LdFVx(u8),        // Fx29 - LD F, Vx
+    LdBVx(u8),        // Fx33 - LD B, Vx
+    LdIVx(u8),        // Fx55 - LD [I], Vx
+    LdVxI(u8),        // Fx65 - LD Vx, [I]
+}
+
+impl OpCode {
+    pub fn decode(raw_a: u8, raw_b: u8) -> Self {
+        let opcode: u16 = (raw_a as u16) << 8 | (raw_b as u16);
+
+        match opcode & 0xF000 {
+            0x0000 => match opcode & 0x000F {
+                0x0000 => Self::Cls,
+                0x000E => Self::Ret,
+                _ => unreachable!("Invalid Op {:#x}", opcode),
+            },
+            0x1000 => Self::Jp(opcode & 0x0FFF),
+            0x2000 => Self::Call(opcode & 0x0FFF),
+            0x3000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let value = opcode & 0x00FF;
+                Self::SeVx(vx, value)
+            }
+            0x4000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let value = opcode & 0x00FF;
+                Self::SNeVx(vx, value)
+            }
+            0x5000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let vy = ((opcode & 0x00F0) >> 4) as u8;
+                Self::SeVxVy(vx, vy)
+            }
+            0x6000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let value = opcode & 0x00FF;
+                Self::LdVx(vx, value)
+            }
+            0x7000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let value = opcode & 0x00FF;
+                Self::AddVx(vx, value)
+            }
+            0x8000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let vy = ((opcode & 0x00F0) >> 4) as u8;
+
+                match opcode & 0x000F {
+                    0x0000 => Self::LdVxVy(vx, vy),
+                    0x0001 => Self::Or(vx, vy),
+                    0x0002 => Self::And(vx, vy),
+                    0x0003 => Self::Xor(vx, vy),
+                    0x0004 => Self::Add(vx, vy),
+                    0x0005 => Self::Sub(vx, vy),
+                    0x0006 => Self::Shr(vx),
+                    0x0007 => Self::SubN(vx, vy),
+                    0x000E => Self::Shl(vx),
+                    _ => unreachable!("Invalid Op {:#x}", opcode),
+                }
+            }
+            0x9000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let vy = ((opcode & 0x00F0) >> 4) as u8;
+                Self::SeNeVxVy(vx, vy)
+            }
+            0xA000 => Self::LdI(opcode & 0x0FFF),
+            0xB000 => Self::JpV0(opcode & 0x0FFF),
+            0xC000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let value = (opcode & 0x00FF) as u8;
+                Self::Rnd(vx, value)
+            }
+            0xD000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                let vy = ((opcode & 0x00F0) >> 4) as u8;
+                let n = (opcode & 0x000F) as u8;
+                Self::Drw(vx, vy, n)
+            }
+            0xE000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                match opcode & 0x00FF {
+                    0x009E => Self::Skp(vx),
+                    0x00A1 => Self::SkNp(vx),
+                    _ => unreachable!("Invalid Op {:#x} {:#x}", opcode, opcode & 0x00FF),
+                }
+            }
+            0xF000 => {
+                let vx = ((opcode & 0x0F00) >> 8) as u8;
+                match opcode & 0x00FF {
+                    0x0007 => Self::LdVxDt(vx),
+                    0x000A => Self::LdVxK(vx),
+                    0x0015 => Self::LdDtVx(vx),
+                    0x0018 => Self::LdStVx(vx),
+                    0x001E => Self::AddIVx(vx),
+                    0x0029 => Self::LdFVx(vx),
+                    0x0033 => Self::LdBVx(vx),
+                    0x0055 => Self::LdIVx(vx),
+                    0x0065 => Self::LdVxI(vx),
+                    _ => unreachable!("Invalid Op {:#x}", opcode),
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
